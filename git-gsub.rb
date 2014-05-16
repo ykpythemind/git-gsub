e648f0826078a94237ba60ea087dfdde08da85f0
Katsusuke Shimizu <k-shimizu@mksc.jp>
2014-05-16 10:41:26 +0900

    Add file name masking function.

--- git-gsub.rb
+++ git-gsub.rb
@@ -1,30 +1,43 @@
 #!/usr/bin/env ruby
 
 begin
-  raise "Invalid argument" if ARGV.size != 2
+  raise "Invalid argument" if ARGV.size != 2 and ARGV.size != 3
 
-  from_str, to_str = ARGV
+  from_str, to_str, file_mask_str = ARGV
   begin
     from = eval(from_str)
   rescue
     from = from_str
   end
 
+  file_mask = nil
+  begin
+    if file_mask_str
+      file_mask = eval(file_mask_str)
+    end
+  rescue
+    file_mask = file_mask_str
+  end
+
   print "from:"
-  p from
+  p from: from, to: to_str, file_mask: file_mask
 
   files = `git ls-files`
   files.lines.each do |path|
     path.strip!
+    if file_mask
+      unless path.index file_mask
+        next
+      end
+    end
+
     buf = nil
     open(path) do |f|
       old_buf = f.read
       begin
-        if from =~ old_buf
-          print "match:#{path}"
-          buf = eval("old_buf.gsub(from, \"#{to_str}\")")
-          buf = nil if old_buf == buf
-        end
+        p path: path, eval: "old_buf.gsub(from, \"#{to_str}\")"
+        buf = old_buf.gsub(from) { eval(to_str) }
+        buf = nil if old_buf == buf
       rescue => ex
         #puts "path:#{path}"
         #p ex
@@ -42,9 +55,10 @@ rescue => ex
   puts ex.message
   print <<EOS
 Usage:
-ruby #{File.basename(__FILE__)} from to
-from  - gsub from string or ruby script
-to    - gsub to string or ruby script
+ruby #{File.basename(__FILE__)} from to [file_mask]
+from      - gsub from string or ruby script
+to        - gsub to string or ruby script
+file_mask - File name or file name matching ruby script
 
 example:
 git gsub hoge piyo
